19b1b9a2f7551232ac9291389eba1038af50ca81
Avery Burns <a.aburns95@gmail.com>
2024-09-25 17:51:28 -0700

    new resume, alt text updated

--- app/src/components/pages/About.js
+++ app/src/components/pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CodeBar from "../CodeBar";
 import "../../css/About.css";
-import resume from "../../images/A. Aaron Burns - Resume.pdf";
+import resume from "../../images/AveryAaronBurnsResume.pdf";
 
 const About = () => {
   return (
@@ -15,10 +15,21 @@ const About = () => {
           alt="Aaron Burns"
         />
         <p>
-          I am a Software Engineer with diverse experience in developer evangelism and common services engineering. Built developer communities, created technical content, and introduced new API/SDK features as a Webex Developer Evangelist. This honed my communication and technical skills, enabling me to connect with audiences and explain complex concepts clearly. As a Common Services Engineer, I tackled process streamlining, innovative solutions, and issue resolution, solidifying my problem-solving and teamwork abilities.
+          I am a Software Engineer with diverse experience in developer
+          evangelism and common services engineering. Built developer
+          communities, created technical content, and introduced new API/SDK
+          features as a Webex Developer Evangelist. This honed my communication
+          and technical skills, enabling me to connect with audiences and
+          explain complex concepts clearly. As a Common Services Engineer, I
+          tackled process streamlining, innovative solutions, and issue
+          resolution, solidifying my problem-solving and teamwork abilities.
         </p>
         <p>
-          Now, I'm eager to learn and grow in a way that leverages my existing skills and expands my potential. I'm particularly interested in opportunities that allow me to combine my technical expertise with my passion for community building and user experience, contributing to the development of cutting-edge software solutions.
+          Now, I'm eager to learn and grow in a way that leverages my existing
+          skills and expands my potential. I'm particularly interested in
+          opportunities that allow me to combine my technical expertise with my
+          passion for community building and user experience, contributing to
+          the development of cutting-edge software solutions.
         </p>
         <a href={resume} alt="resume" className="resume">
           Resume
@@ -27,7 +38,7 @@ const About = () => {
         <br />
         <a
           href="https://www.credly.com/badges/6d1bd084-082c-4e61-9035-241f1d042e54/public_url"
-          alt="Lambda School Full-Stack Certiicate"
+          alt="Bloom Institute Full-Stack Certiicate"
           className="resume"
         >
           Bloom Institute of Technology Certificate
